dfc26dc3d593a81ebfd1bd31754e2427280cd77b
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2024-10-29 21:02:59 -0300

    Add files via upload

---
+++ projeto04/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react';
+import ConversordeMoedas from './ConversordeMoedas';
+
+function App() {
+
+
+  return (
+    <div class name="App">
+
+      <ConversordeMoedas />
+
+    </div>
+  );
+}
+
+export default App 
